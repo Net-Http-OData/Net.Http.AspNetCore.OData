ebecfa15e28792f2459289a1c3968b6798d56f43
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-02-02 23:05:40 +0000

    Added a local build script and updated azure-pipeline build

---
+++ build.ps1
@@ -0,0 +1,12 @@
+# A script for building, testing and viewing code coverage locally
+dotnet build
+
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=.\TestResults\
+
+Set-Location Net.Http.AspNetCore.OData.Tests
+
+dotnet reportgenerator "-reports:TestResults\coverage.cobertura.xml" "-targetdir:TestResults\html" -reporttypes:HTML;
+
+Start-Process "TestResults\html\index.htm"
+
+Set-Location ..
