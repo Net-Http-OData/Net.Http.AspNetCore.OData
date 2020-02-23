715e76ce0f5bbbbe1161d7a5f96d11d23d6957c5
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-02-23 19:17:35 +0000

    Added ODataMetadataController

---
+++ Net.Http.AspNetCore.OData.Tests/Metadata/ODataMetadataControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Net.Http.AspNetCore.OData.Metadata;
+using Xunit;
+
+namespace Net.Http.AspNetCore.OData.Tests.Metadata
+{
+    public class ODataMetadataControllerTests
+    {
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void GetReturnsCsdlXmlDocument()
+        {
+            TestHelper.EnsureEDM();
+
+            var controller = new ODataMetadataController();
+
+            IActionResult response = controller.Get();
+
+            Assert.IsType<ContentResult>(response);
+
+            var contentResult = (ContentResult)response;
+
+            string result = contentResult.Content;
+
+            Assert.NotNull(result);
+
+            var resultXml = XDocument.Parse(result);
+
+            Assert.Equal("Edmx", resultXml.Root.Name.LocalName);
+        }
+    }
+}
---
+++ Net.Http.AspNetCore.OData/Metadata/ODataMetadataController.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ODataMetadataController.cs" company="Project Contributors">
+// Copyright Project Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Net.Http.OData;
+using Net.Http.OData.Metadata;
+using Net.Http.OData.Model;
+
+namespace Net.Http.AspNetCore.OData.Metadata
+{
+    /// <summary>
+    /// An API controller which exposes the OData service metadata.
+    /// </summary>
+    [ApiController]
+    [Route("odata")]
+    public sealed class ODataMetadataController : ControllerBase
+    {
+        private static string s_metadataXml;
+
+        /// <summary>
+        /// Gets the <see cref="IActionResult"/> which contains the service metadata.
+        /// </summary>
+        /// <returns>The <see cref="IActionResult"/> which contains the service metadata.</returns>
+        [HttpGet]
+        [Route("$metadata")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            if (s_metadataXml is null)
+            {
+                using (var stringWriter = new Utf8StringWriter())
+                {
+                    XDocument metadataDocument = XmlMetadataProvider.Create(EntityDataModel.Current, ODataServiceOptions.Default);
+                    metadataDocument.Save(stringWriter);
+
+                    s_metadataXml = stringWriter.ToString();
+                }
+            }
+
+            return Content(s_metadataXml, "application/xml", Encoding.UTF8);
+        }
+    }
+}
