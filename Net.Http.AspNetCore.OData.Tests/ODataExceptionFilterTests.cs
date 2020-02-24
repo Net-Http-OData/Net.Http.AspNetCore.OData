40139a5a14a4ace264ecf9ec28984f99fa4006c0
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-02-24 13:48:33 +0000

    Added ODataExceptionFilter

---
+++ Net.Http.AspNetCore.OData.Tests/ODataExceptionFilterTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Net.Http.OData;
+using Xunit;
+
+namespace Net.Http.AspNetCore.OData.Tests
+{
+    public class ODataExceptionFilterTests
+    {
+        [Fact]
+        public void OnException_DoesNotSet_Result_If_Exception_IsNot_ODataException()
+        {
+            var httpRequest = TestHelper.CreateHttpRequest("/OData/Products");
+
+            var exceptionContext = new ExceptionContext(new ActionContext(httpRequest.HttpContext, new RouteData(), new ActionDescriptor()), new IFilterMetadata[0])
+            {
+                HttpContext = httpRequest.HttpContext,
+                Exception = new ArgumentNullException("Name"),
+            };
+
+            var exceptionFilter = new ODataExceptionFilter();
+            exceptionFilter.OnException(exceptionContext);
+
+            Assert.Null(exceptionContext.Result);
+            Assert.False(exceptionContext.ExceptionHandled);
+        }
+
+        [Fact]
+        public void OnException_Sets_Result_If_Exception_Is_ODataException()
+        {
+            var httpRequest = TestHelper.CreateHttpRequest("/OData/Products");
+
+            var exceptionContext = new ExceptionContext(new ActionContext(httpRequest.HttpContext, new RouteData(), new ActionDescriptor()), new IFilterMetadata[0])
+            {
+                HttpContext = httpRequest.HttpContext,
+                Exception = new ODataException("The type 'NorthwindModel.Product' does not contain a property named 'Foo'", HttpStatusCode.BadRequest),
+            };
+
+            var exceptionFilter = new ODataExceptionFilter();
+            exceptionFilter.OnException(exceptionContext);
+
+            Assert.NotNull(exceptionContext.Result);
+            Assert.True(exceptionContext.ExceptionHandled);
+        }
+    }
+}
---
+++ Net.Http.AspNetCore.OData/ODataExceptionFilter.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ODataExceptionFilter.cs" company="Project Contributors">
+// Copyright Project Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+using Microsoft.AspNetCore.Mvc.Filters;
+using Net.Http.OData;
+
+namespace Net.Http.AspNetCore.OData
+{
+    /// <summary>
+    /// An <see cref="IExceptionFilter"/> which returns the correct response for an <see cref="ODataException"/>.
+    /// </summary>
+    public sealed class ODataExceptionFilter : IExceptionFilter
+    {
+        /// <summary>
+        /// Called after an action has thrown an System.Exception.
+        /// </summary>
+        /// <param name="context">The <see cref="ExceptionContext"/>.</param>
+        public void OnException(ExceptionContext context)
+        {
+            if (context?.Exception is ODataException odataException)
+            {
+                context.Result = context.HttpContext.Request.CreateODataErrorResult(odataException);
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
