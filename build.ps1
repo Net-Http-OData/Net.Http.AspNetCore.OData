54a42c91590d9ecac5e05444d57c51b074ff1784
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-02-28 12:25:07 +0000

    Updated how the code coverage is run

--- build.ps1
+++ build.ps1
@@ -1,10 +1,13 @@
 # A script for building, testing and viewing code coverage locally
 $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
-$testResults = Join-Path $scriptPath -ChildPath '\Net.Http.OData.Tests\TestResults'
+$testResults = Join-Path $scriptPath -ChildPath '\Net.Http.AspNetCore.OData.Tests\TestResults'
 
 if (Test-Path $testResults){
     Remove-Item -Path $testResults -Recurse
 }
+
+dotnet tool uninstall dotnet-reportgenerator-globaltool --global
+dotnet tool install --global dotnet-reportgenerator-globaltool
 
 dotnet clean
 dotnet build
@@ -12,7 +15,7 @@ dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:Coverle
 
 Set-Location Net.Http.AspNetCore.OData.Tests
 
-dotnet reportgenerator "-reports:TestResults\coverage.cobertura.xml" "-targetdir:TestResults\Coverage" -reporttypes:HTML;
+reportgenerator "-reports:TestResults\coverage.cobertura.xml" "-targetdir:TestResults\Coverage" -reporttypes:HTML;
 
 Start-Process "TestResults\Coverage\index.htm"
 
