703a9f4a5ebf814c4c1d652c14b08cd6c4ec28d9
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-03-24 12:46:29 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,8 @@
 # A script for building, testing and viewing code coverage locally
+param(
+    [bool]$updateReportGenTool = $false,
+    [bool]$genCoverage = $true
+)
 $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
 $testResults = Join-Path $scriptPath -ChildPath '\Net.Http.AspNetCore.OData.Tests\TestResults'
 
@@ -6,17 +10,17 @@ if (Test-Path $testResults){
     Remove-Item -Path $testResults -Recurse
 }
 
-dotnet tool uninstall dotnet-reportgenerator-globaltool --global
-dotnet tool install --global dotnet-reportgenerator-globaltool
+if ($updateReportGenTool) {
+    dotnet tool update --global dotnet-reportgenerator-globaltool
+}
 
 dotnet clean
 dotnet build
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=.\TestResults\
 
-Set-Location Net.Http.AspNetCore.OData.Tests
-
-reportgenerator "-reports:TestResults\coverage.cobertura.xml" "-targetdir:TestResults\Coverage" -reporttypes:HTML;
-
-Start-Process "TestResults\Coverage\index.htm"
-
-Set-Location ..
+if ($genCoverage) {
+    Set-Location Net.Http.AspNetCore.OData.Tests
+    reportgenerator "-reports:TestResults\coverage.cobertura.xml" "-targetdir:TestResults\Coverage" -reporttypes:HTML
+    Start-Process "TestResults\Coverage\index.htm"
+    Set-Location ..
+}
