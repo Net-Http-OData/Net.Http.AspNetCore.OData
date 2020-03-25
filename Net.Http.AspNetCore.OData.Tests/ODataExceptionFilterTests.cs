b6e2d61ab04c2992d5dd3cc1fd99fbcc882c9ef0
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-03-25 16:18:40 +0000

    Update ODataExceptionFilterTests.cs

--- Net.Http.AspNetCore.OData.Tests/ODataExceptionFilterTests.cs
+++ Net.Http.AspNetCore.OData.Tests/ODataExceptionFilterTests.cs
@@ -31,6 +31,24 @@ namespace Net.Http.AspNetCore.OData.Tests
         }
 
         [Fact]
+        public void OnException_DoesNotSet_Result_If_Exception_IsNull()
+        {
+            HttpRequest request = TestHelper.CreateODataHttpRequest("/OData/Products");
+
+            var exceptionContext = new ExceptionContext(new ActionContext(request.HttpContext, new RouteData(), new ActionDescriptor()), new IFilterMetadata[0])
+            {
+                HttpContext = request.HttpContext,
+                Exception = null,
+            };
+
+            var exceptionFilter = new ODataExceptionFilter();
+            exceptionFilter.OnException(exceptionContext);
+
+            Assert.Null(exceptionContext.Result);
+            Assert.False(exceptionContext.ExceptionHandled);
+        }
+
+        [Fact]
         public void OnException_Sets_Result_If_Exception_Is_ODataException()
         {
             HttpRequest request = TestHelper.CreateODataHttpRequest("/OData/Products");
